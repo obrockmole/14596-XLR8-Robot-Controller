982d9cb4fe053e8cf903509dcc4d2d9a62d52f28
DeadlyTime <86027400+obrockmole@users.noreply.github.com>
2023-07-03 15:13:19 -0400

    Moved testing programs from TeleOp to Testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/ExternalStorage_Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/ExternalStorage_Test.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.TeleOp;
+package org.firstinspires.ftc.teamcode.Testing;
 
 import android.os.Environment;
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/VisionTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/VisionTest.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.TeleOp;
+package org.firstinspires.ftc.teamcode.Testing;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
