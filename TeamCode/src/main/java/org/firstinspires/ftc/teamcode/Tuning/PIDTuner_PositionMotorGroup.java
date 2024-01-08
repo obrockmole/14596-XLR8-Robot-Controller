9b5aa061d3a74ae4fcdd09c0b26e4eb2b8b405f0
obrockmole <86027400+obrockmole@users.noreply.github.com>
2024-01-08 10:56:04 -0500

    Update tuning opmodes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tuning/PIDTuner_PositionMotorGroup.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tuning/PIDTuner_PositionMotorGroup.java
@@ -3,45 +3,51 @@ package org.firstinspires.ftc.teamcode.Tuning;
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 
 import org.firstinspires.ftc.teamcode.Systems.Motors.Motor;
+import org.firstinspires.ftc.teamcode.Systems.Motors.MotorGroup;
 import org.firstinspires.ftc.teamcode.Systems.Motors.MotorList;
 
 //@Disabled
 @Config
 @TeleOp(group = "Tuning")
-public class PIDTuner_PositionMotor extends OpMode {
-    private Motor motor;
+public class PIDTuner_PositionMotorGroup extends OpMode {
+    private MotorGroup motors;
 
-    public static String MOTOR_NAME = "positionMotor";
+    public static String MOTOR_1_NAME = "positionMotor1";
+    public static String MOTOR_2_NAME = "positionMotor2";
 
     public static MotorList MOTOR_TYPE = MotorList.GOBILDA_435;
     public static PIDFCoefficients PIDF_COEFFICIENTS = new PIDFCoefficients(0, 0, 0, 0);
     public static int TARGET_POSITION = 0;
     public static int TOLERANCE = 0;
-    public static boolean REVERSED = false;
+    public static  boolean MOTOR_1_REVERSED = false;
+    public static  boolean MOTOR_2_REVERSED = false;
 
     @Override
     public void init() {
-        motor = new Motor(hardwareMap, MOTOR_NAME, MOTOR_TYPE, Motor.Mode.POSITION, TOLERANCE, REVERSED);
+        motors = new MotorGroup(
+                new Motor(hardwareMap, MOTOR_1_NAME, MOTOR_TYPE, Motor.Mode.POSITION, TOLERANCE, MOTOR_1_REVERSED),
+                new Motor(hardwareMap, MOTOR_2_NAME, MOTOR_TYPE, Motor.Mode.POSITION, TOLERANCE, MOTOR_2_REVERSED)
+        );
 
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
     }
 
     @Override
     public void loop() {
-        if (motor.getMotorType() != MOTOR_TYPE) motor.setMotorType(MOTOR_TYPE);
+        if (motors.getMotorType() != MOTOR_TYPE) motors.setMotorType(MOTOR_TYPE);
 
-        motor.setPIDF(PIDF_COEFFICIENTS.p, PIDF_COEFFICIENTS.i, PIDF_COEFFICIENTS.d, PIDF_COEFFICIENTS.f)
+        motors.setPIDF(PIDF_COEFFICIENTS.p, PIDF_COEFFICIENTS.i, PIDF_COEFFICIENTS.d, PIDF_COEFFICIENTS.f)
                 .setTargetPosition(TARGET_POSITION)
                 .setTolerance(TOLERANCE)
-                .setReversed(REVERSED)
+                .setReversed(0, MOTOR_1_REVERSED)
+                .setReversed(1, MOTOR_2_REVERSED)
                 .update();
 
-        motor.log(telemetry, hardwareMap);
+        motors.log(telemetry, hardwareMap);
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tuning/PositionTuner_PositionServoGroup.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode.Tuning;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Systems.Servos.PositionServo;
+import org.firstinspires.ftc.teamcode.Systems.Servos.PositionServoGroup;
+
+//@Disabled
+@Config
+@TeleOp(group = "Tuning")
+public class PositionTuner_PositionServoGroup extends OpMode {
+    private PositionServoGroup servos;
+
+    public static String SERVO_1_NAME = "positionServo1";
+    public static String SERVO_2_NAME = "positionServo1";
+
+    public static double TARGET_POSITION = 0;
+    public static double MIN_SCALE = 0;
+    public static double MAX_SCALE = 1;
+    public static boolean SERVO_1_REVERSED = false;
+    public static boolean SERVO_2_REVERSED = false;
+
+    @Override
+    public void init() {
+        servos = new PositionServoGroup(
+                new PositionServo(hardwareMap, SERVO_1_NAME, MIN_SCALE, MAX_SCALE, SERVO_1_REVERSED),
+                new PositionServo(hardwareMap, SERVO_2_NAME, MIN_SCALE, MAX_SCALE, SERVO_2_REVERSED)
+        );
+        servos.setTargetPosition(TARGET_POSITION);
+
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+    }
+
+    @Override
+    public void loop() {
+        servos.setTargetPosition(TARGET_POSITION)
+                .setReversed(0, SERVO_1_REVERSED)
+                .setReversed(1, SERVO_2_REVERSED)
+                .setScaleRange(MIN_SCALE, MAX_SCALE)
+                .update();
+
+        servos.log(telemetry, hardwareMap);
+    }
+}
